0e895f9671d9d3aa3bf72bc6acbae2b2fa76f282
void237 <159345142+void237@users.noreply.github.com>
2024-03-13 11:09:45 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,4 @@
-/**
-* Template Name: KnightOne
-* Updated: Mar 12 2024 with Bootstrap v5.3.3
-* Template URL: https://bootstrapmade.com/knight-simple-one-page-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 (function() {
   "use strict";
 
